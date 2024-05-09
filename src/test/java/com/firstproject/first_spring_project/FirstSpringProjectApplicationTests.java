f1ecb037a73e4a311ea7dfdb81b4318c5bb7baf5
Manan Pathak <pathakmanan5@gmail.com>
2024-05-08 22:07:22 -0400

    first commit

---
+++ src/test/java/com/firstproject/first_spring_project/FirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstproject.first_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
